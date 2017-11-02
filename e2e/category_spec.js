0bd6460600647a04a8a60c00e6077433f2e0164f
Johan Blom <johan.blom@flextrade.com>
2017-11-02 18:31:35 +0000

    added a test to check you can't add a category twice

--- e2e/category_spec.js
+++ e2e/category_spec.js
@@ -26,41 +26,58 @@ describe('category page', function() {
   it('should allow you to update a category', function() {
     var btnElement, txtElement, txtElementText;
     browser.get('http://localhost:4000/#!/category');
-    element.all(by.repeater('item in categories')).then(function(items) {
+    element.all(by.repeater('item in categories')).then(function (items) {
       btnElement = items[0].element(by.buttonText('Edit'));
       txtElement = items[0].element(by.className('name'));
       txtElement.getText().then(function (text) {
         txtElementText = text;
-      }).then(function() {
+      }).then(function () {
         btnElement.click();
         var input = element(by.model('editedCategory.name'));
-        expect (input.getAttribute('value')).toBe(txtElementText);
+        expect(input.getAttribute('value')).toBe(txtElementText);
 
-      });
-    });
-
-    it('should allow you to remove a category', function() {
-      var btnElement, txtElement, txtElementText;
-      browser.get('http://localhost:4000/#!/category');
-      element.all(by.repeater('item in categories')).then(function(items) {
-        btnElement = items[0].element(by.buttonText('Delete'));
-        txtElement = items[0].element(by.className('name ng-binding'));
-        txtElement.getText().then(function (text) {
-          txtElementText = text;
-        });
-        btnElement.click();
-      });
-
-      category = element.all(by.repeater('item in categories')).then(function(items) {
-        items.map(function(item) {
-          item.getText().then(function(text) {
-            expect (text).not.toContain(txtElementText);
-          });
-        })
       });
     });
   });
+
   it('should not allow you to add a category twice', function() {
+    var txtElement, txtElementText;
+    element.all(by.repeater('item in categories')).then(function (items) {
+      txtElement = items[1].element(by.className('name'));
+      txtElement.getText().then(function (text) {
+        txtElementText = text;
+      }).then(function () {
+        element(by.model('editedCategory.name')).clear().then(function() {
+        element(by.model('editedCategory.name')).sendKeys(txtElementText);
+        })
+      }).then(function() {
+        var createBtn = element(by.buttonText('Create'));
+        browser.sleep(10);
+        expect(createBtn.isEnabled()).toBe(false);
+      });
+    })
+  });
+
+
+  it('should allow you to remove a category', function() {
+    var btnElement, txtElement, txtElementText;
+    browser.get('http://localhost:4000/#!/category');
+    element.all(by.repeater('item in categories')).then(function(items) {
+      btnElement = items[0].element(by.buttonText('Delete'));
+      txtElement = items[0].element(by.className('name ng-binding'));
+      txtElement.getText().then(function (text) {
+        txtElementText = text;
+      });
+      btnElement.click();
+    });
+
+    category = element.all(by.repeater('item in categories')).then(function(items) {
+      items.map(function(item) {
+        item.getText().then(function(text) {
+          expect (text).not.toContain(txtElementText);
+        });
+      })
+    });
   });
 });
 
