bacb1487500a8bdfcc8cec030ba764defab23791
Johan Blom <johan.blom@flextrade.com>
2017-11-13 16:13:54 +0000

    added test for invalid login

--- e2e/login_spec.js
+++ e2e/login_spec.js
@@ -1,16 +1,23 @@
-beforeAll(function(done) {
-  done();
-});
-
 describe('login page', function() {
   it('should not allow you to add a category', function() {
     element(by.css('a[href*="#!/logout"')).click();
     browser.get('http://localhost:4000/#!/category');
-    expect (element(by.css('h1')).getText()).toContain('403 Forbidden');
+    expect(element(by.css('h1')).getText()).toContain('403 Forbidden');
   })
-});
 
-describe('login page', function() {
+  it('should not login invalid users', function() {
+    browser.get('http://localhost:4000');
+    element(by.model('user.email')).clear();
+    element(by.model('user.password')).clear();
+    element(by.model('user.email')).sendKeys('blah');
+    element(by.model('user.password')).sendKeys('blah');
+    var loginBtn = element(by.css('.login-button'));
+    browser.sleep(500);
+    loginBtn.click();
+    browser.sleep(500);
+    expect(element(by.css('.error-msg')).getText()).toContain('Login failed, please try again');
+  })
+
   it('should login valid users', function() {
     browser.get('http://localhost:4000');
     element(by.model('user.email')).clear();
