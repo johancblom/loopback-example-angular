1424b0f896abb6be39cc3079494ee6933b1c39d2
Johan Blom <johan.blom@flextrade.com>
2017-11-22 13:52:46 +0000

    migrated to postgres database

--- server/boot/sample-model.js
+++ server/boot/sample-model.js
@@ -1,14 +1,24 @@
 var async = require('async');
 
 module.exports = function(app) {
-  var users = {};
-  app.models.User.create([
-    {email: 'foo@bar.com', password: 'foobar'},
-    {email: 'bar@foo.com', password: 'barfoo'}
-  ], function (err, results) {
-    console.log(results);
-    users = results;
-  });
 
+  // app.dataSources.db.automigrate('Todo', function(err) {
+  //   console.log(err);
+  // });
+  // app.dataSources.db.automigrate('Category', function(err) {
+  //   console.log(err);
+  // });
+  // var users = {};
+  // app.models.User.create([
+  //   {email: 'foo@bar.com', password: 'foobar'},
+  //   {email: 'bar@foo.com', password: 'barfoo'}
+  // ], function (err, results) {
+  //   console.log(results);
+  //   users = results;
+  // });
+
+  // app.dataSources.db.automigrate(['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'], function(err) {
+  //   console.log(err);
+  // });
 }
 
